cbf0a6c87190680b9701ad6890245d0428296a8e
Raven <official.ravendelarama@gmail.com>
2024-07-31 23:37:17 +0800

    testing loading page

---
+++ src/app/(auth)/loading.tsx
@@ -0,0 +1,7 @@
+export default function AuthLoading() {
+  return (
+    <div className="bg-background w-full h-screen flex flex-col justify-center items-center">
+      Loading...
+    </div>
+  );
+}
